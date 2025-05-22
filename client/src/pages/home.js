070f414ccee8f759658322f4286bb4550aba3abb
Aditya Pillai <adi4pillai@gmail.com>
2025-05-22 12:44:05 -0700

    react: multiplayer options

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,10 +1,20 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
 import Gallery from "../components/Gallery/Gallery";
 
 export default function Home() {
   const userData = JSON.parse(localStorage.getItem("userData"));
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleOptionClick = (option) => {
+    console.log(`You chose ${option}`);
+
+    //TODO - connect to websocket server here (then add transfer to /multipaint here as well, or keep it in <Link>?)
+
+    setShowPopup(false);
+  };
+
   return (
     <Container>
       <div className="mb-5 mt-5">
@@ -16,6 +26,33 @@ export default function Home() {
           Start Painting 🖌
         </Button>
       </Link>
+
+      <Button variant="secondary" onClick={() => setShowPopup(true)}>
+        Choose an Option
+      </Button>
+
+      <Modal show={showPopup} onHide={() => setShowPopup(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Select an Option</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Link to="/multipaint" className="me-5">
+            <Button
+              variant="outline-primary"
+              className="me-2"
+              onClick={() => handleOptionClick("create session")}
+            >
+              Create session
+            </Button>
+          </Link>
+          <Button
+            variant="outline-success"
+            onClick={() => handleOptionClick("join session")}
+          >
+            Join session
+          </Button>
+        </Modal.Body>
+      </Modal>
       <Gallery/>
     </Container>
   )
